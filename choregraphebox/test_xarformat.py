e37e6afbd32e4b702ad64328633df9948a4d0f11
yacchin1205 <yazawa@yzwlab.net>
2014-12-31 19:49:20 +0900

    Add unittest for loaded box

--- choregraphebox/test_xarformat.py
+++ choregraphebox/test_xarformat.py
@@ -51,6 +51,31 @@ class TestXARformat(unittest.TestCase):
 
         self.assertEquals(len(items[2].get_children()), 0)
 
+    def test_load_box(self):
+        boxlib = xarformat.load(os.path.join(self.resources, "boxlib2"))
+        items = sorted(boxlib.get_children(), key=lambda item: item.name)
+        self.assertEquals(len(items), 1)
+        self.assertEquals(items[0].name, "Test1")
+        box = items[0].get_box()
+        self.assertEquals(box.name, "Test1")
+        ports = box.get_inputs()
+        self.assertEquals(len(ports), 5)
+        self.assertEquals(ports[0].name, "onLoad")
+        self.assertEquals(ports[1].name, "onStart")
+        self.assertEquals(ports[2].name, "onStop")
+        self.assertEquals(ports[3].name, "input1")
+        self.assertEquals(ports[4].name, "input2")
+        ports = box.get_outputs()
+        self.assertEquals(len(ports), 3)
+        self.assertEquals(ports[0].name, "onStopped")
+        self.assertEquals(ports[1].name, "output1")
+        self.assertEquals(ports[2].name, "output2")
+        params = box.get_parameters()
+        self.assertEquals(len(params), 3)
+        self.assertEquals(params[0].name, "param1")
+        self.assertEquals(params[1].name, "param2")
+        self.assertEquals(params[2].name, "param3")
+
 
 if __name__ == '__main__':
     unittest.main()
