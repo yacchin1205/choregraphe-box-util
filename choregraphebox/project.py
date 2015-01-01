276fd5b06e6f2d1661a0ad97a4808a39f6088187
yacchin1205 <yazawa@yzwlab.net>
2015-01-01 13:05:46 +0900

    Fix PEP8 issues

--- choregraphebox/project.py
+++ choregraphebox/project.py
@@ -17,23 +17,27 @@ class Project:
         self.behaviors = []
         for desc in pml.findall(".//BehaviorDescription"):
             if desc.attrib["name"] == "behavior":
-                behavior_path = os.path.join(os.path.dirname(file), desc.attrib["src"])
-                self.behaviors.append(xarformat.load(os.path.join(behavior_path, desc.attrib["xar"])))
-                
+                behavior_path = os.path.join(os.path.dirname(file),
+                                             desc.attrib["src"])
+                self.behaviors.append(
+                    xarformat.load(os.path.join(behavior_path,
+                                   desc.attrib["xar"])))
+
     def add_boxlib(self, boxlib):
         self.boxlibs.append(boxlib)
-        
+
     def replace(self):
         for behavior in self.behaviors:
             for box in behavior.get_box().get_all_boxes():
                 base = self._find_box(box.name)
                 if base:
                     box.copy_from(base.get_box())
-                    
+
     def save(self):
         for behavior in self.behaviors:
-            behavior.xarxml.write(behavior.file, encoding="utf-8", xml_declaration=True, method="xml")
-        
+            behavior.xarxml.write(behavior.file, encoding="utf-8",
+                                  xml_declaration=True, method="xml")
+
     def _find_box(self, name):
         for boxlib in self.boxlibs:
             box = boxlib.find(name)
