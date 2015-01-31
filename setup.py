5f63a25dd815daae8e90ba57b105f1930e61a8b0
yacchin1205 <yazawa@yzwlab.net>
2015-01-31 10:15:27 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='ChoregrapheBoxUtils',
+      version='0.1',
+      description='Choregraphe Box Utilities',
+      author='Satoshi Yazawa',
+      author_email='yazawa@yzwlab.net',
+      url='https://github.com/yacchin1205/choregraphe-box-util',
+      packages=['choregraphebox'],
+     )
