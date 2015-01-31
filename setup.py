068dcc25e202afe60fc380229f8d4b3ecb094837
yacchin1205 <yazawa@yzwlab.net>
2015-01-31 14:41:03 +0900

    Add dependencies to setup.py

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(name='ChoregrapheBoxUtils',
       author_email='yazawa@yzwlab.net',
       url='https://github.com/yacchin1205/choregraphe-box-util',
       packages=['choregraphebox'],
+      install_requires=["lxml"],
       entry_points={'console_scripts':
                     ['verify-boxlib=choregraphebox.boxlib:verify'], },
       )
